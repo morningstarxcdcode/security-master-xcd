aa31bcc9d116cd0b009cc34e6787ccee686da1cb
Sourav <205398826+morningstarxcdcode@users.noreply.github.com>
2025-08-03 19:44:52 +0530

    Delete Documents/aibuddy directory

--- Documents/aibuddy/examples/example_usage.py
+++
@@ -1,6 +0,0 @@
-# Example usage of the AI Buddy package
-from aibuddy import core
-
-# Example function call
-result = core.some_function()
-print(result)
--- Documents/aibuddy/scripts/deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Deployment script
-
-echo "Deploying AI Buddy..."
-# Add deployment commands here
